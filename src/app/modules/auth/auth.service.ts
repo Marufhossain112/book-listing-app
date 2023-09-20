37e64c1af0a636c72422adcd32f1b91bc3a76e33
maruf <dev.maruf112@gmail.com>
2023-09-20 09:35:42 +0600

    password hashed

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,10 +1,18 @@
 import { User } from '@prisma/client';
 import { prisma } from '../../../shared/prisma';
 import { selectUserProperties } from './auth.constants';
+import bcrypt from 'bcrypt';
 
 const signUp = async (data: User) => {
+  const saltRounds = 10; // You can adjust the number of rounds for security
+  const salt = await bcrypt.genSalt(saltRounds);
+  // Hash the user's password with the generated salt
+  const hashedPassword = await bcrypt.hash(data.password, salt);
   const result = await prisma.user.create({
-    data: data,
+    data: {
+      ...data,
+      password: hashedPassword, // Store the hashed password in the database
+    },
     select: selectUserProperties,
   });
   return result;
